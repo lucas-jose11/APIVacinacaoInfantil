b63231579928211c10a56927c26f251357e4e4be
lucas-jose_da-silva <lucasjds012@gmail.com>
2026-05-19 21:31:12 -0300

    19/05/2026 - Start

---
+++ src/main/java/com/SENAI/apiVacinacaoInfantil/ApiVacinacaoInfantilApplication.java
@@ -0,0 +1,13 @@
+package com.SENAI.apiVacinacaoInfantil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVacinacaoInfantilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVacinacaoInfantilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SENAI/apiVacinacaoInfantil/ApiVacinacaoInfantilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SENAI.apiVacinacaoInfantil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVacinacaoInfantilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
